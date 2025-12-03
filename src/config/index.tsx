002495af3e1fdec02eab47d40ad8b6f1282cfc44
ghr <1919833594@qq.com>
2025-12-03 22:03:28 +0800

    fix(config): v1.2更新API基础URL

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,3 +1,3 @@
 // export const BASE_URL = "http://192.168.112.217:8080";
-export const BASE_URL = "http://localhost:5174";
+export const BASE_URL = import.meta.env.VITE_API_URL;
 export const TIME_OUT = 100000;
